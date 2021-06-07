f4b9654bfbab4decb3dd55c25be2d6a5f3ec363e
Tirdad <abbasi.tirdad@gmail.com>
2021-06-07 18:49:32 +0430

    add new packages and user configuration

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => Tir\User\Entities\User::class,
         ],
 
         // 'users' => [
